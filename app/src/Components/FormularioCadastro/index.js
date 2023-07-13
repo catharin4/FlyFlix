63f934e12373ddb504dc991a6157451f8b871083
Catharina <catharina.bri@gmail.com>
2023-07-13 17:57:39 -0300

    inicio da criaçao da validacao

--- app/src/Components/FormularioCadastro/index.js
+++ app/src/Components/FormularioCadastro/index.js
@@ -1,31 +1,57 @@
+import { useState } from "react"
 import CampoTexto from "../CampoTexto/index.js"
 
-const FormularioCadastro = (nome, cidade, estado, email, senha) => {
+const FormularioCadastro = () => {
+    
+    const [nome, setNome] = useState('')
+    const [cidade, setCidade] = useState('')
+    const [estado, setEstado] = useState('')
+    const [identidade, setIdentidade] = useState('')
+    const [orientacao, setOrientacao] = useState('')
+    const [cor, setCor] = useState('')
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    const aoCadastrar = (evento) => {
+        evento.preventDefault()
+        setNome('')
+        setCidade('')
+        setEstado('')
+        setIdentidade('')
+        setOrientacao('')
+        setCor('')
+        setEmail('')
+        setSenha('')
+    }
+
     return(
         <section className="formulario">
-            <form>
+            <form onSubmit={aoCadastrar}>
                 <CampoTexto
                     obrigatorio={true}
                     label="Nome"
                     placeholder="Digite aqui seu nome"
                     valor={nome}
+                    aoAlterar={valor => setNome(valor)}
                 />
                 <CampoTexto
                     obrigatorio={true}
                     label="Cidade"
                     placeholder="Digite o sua cidade aqui"
                     valor={cidade}
+                    aoAlterar={valor => setCidade(valor)}
                 />
                 <CampoTexto
                     obrigatorio={true}
                     label="Estado"
                     placeholder="Digite o seu estado aqui"
                     valor={estado}
+                    aoAlterar={valor => setEstado(valor)}
                 />
                 <div className="opcoes">
                     <label>
                         Identidade de gênero:
-                        <select name="identidade">
+                        <select name="identidade" value={identidade}>
                             <option value="mulher-cis">Mulher cisgênero</option>
                             <option value="hetero-cis">Homem cisgênero</option>
                             <option value="mulher-trans">Mulher transgênero</option>
@@ -37,7 +63,7 @@ const FormularioCadastro = (nome, cidade, estado, email, senha) => {
                     </label>
                     <label>
                         Orientação sexual:
-                        <select name="orientacao">
+                        <select name="orientacao" value={orientacao}>
                             <option value="heterossexual">Heterossexual</option>
                             <option value="homossexual">Homossexual</option>
                             <option value="bissexual">Bissexual</option>
@@ -48,7 +74,7 @@ const FormularioCadastro = (nome, cidade, estado, email, senha) => {
                     </label>
                     <label>
                         Cor ou raça:
-                        <select name="cor">
+                        <select name="cor" value={cor}>
                             <option value="branco">Branco</option>
                             <option value="preto">Preto</option>
                             <option value="pardo">Pardo</option>
@@ -63,18 +89,14 @@ const FormularioCadastro = (nome, cidade, estado, email, senha) => {
                     label="Email"
                     placeholder="Digite o seu email aqui"
                     valor={email}
+                    aoAlterar={valor => setEmail(valor)}
                 />
                 <CampoTexto
                     obrigatorio={true}
                     label="Senha"
                     placeholder="Digite a sua senha aqui"
                     valor={senha}
-                />
-                <CampoTexto
-                    obrigatorio={true}
-                    label="Confirme a senha"
-                    placeholder="Digite a sua senha aqui"
-                    valor={senha}
+                    aoAlterar={valor => setSenha(valor)}
                 />
             </form>
         </section>    
