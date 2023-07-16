455f0ef21e646fb1643d897160ad4f8b9d4f7e6f
Catharina <catharina.bri@gmail.com>
2023-07-15 21:19:53 -0300

    mudança do formulario e ainda falta terminar essa mudança

--- app/src/Components/FormularioCadastro/index.js
+++ app/src/Components/FormularioCadastro/index.js
@@ -1,107 +1,72 @@
-import { useState } from "react"
-import CampoTexto from "../CampoTexto/index.js"
+import {useFormik} from 'formik'
 
 const FormularioCadastro = () => {
-    
-    const [nome, setNome] = useState('')
-    const [cidade, setCidade] = useState('')
-    const [estado, setEstado] = useState('')
-    const [identidade, setIdentidade] = useState('')
-    const [orientacao, setOrientacao] = useState('')
-    const [cor, setCor] = useState('')
-    const [email, setEmail] = useState('')
-    const [senha, setSenha] = useState('')
-
-    const aoCadastrar = (evento) => {
-        evento.preventDefault()
-        setNome('')
-        setCidade('')
-        setEstado('')
-        setIdentidade('')
-        setOrientacao('')
-        setCor('')
-        setEmail('')
-        setSenha('')
-    }
-
-    return(
-        <section className="formulario">
-            <form onSubmit={aoCadastrar}>
-                <CampoTexto
-                    obrigatorio={true}
-                    label="Nome"
-                    placeholder="Digite aqui seu nome"
-                    valor={nome}
-                    aoAlterar={valor => setNome(valor)}
-                />
-                <CampoTexto
-                    obrigatorio={true}
-                    label="Cidade"
-                    placeholder="Digite o sua cidade aqui"
-                    valor={cidade}
-                    aoAlterar={valor => setCidade(valor)}
-                />
-                <CampoTexto
-                    obrigatorio={true}
-                    label="Estado"
-                    placeholder="Digite o seu estado aqui"
-                    valor={estado}
-                    aoAlterar={valor => setEstado(valor)}
-                />
-                <div className="opcoes">
-                    <label>
-                        Identidade de gênero:
-                        <select name="identidade" value={identidade}>
-                            <option value="mulher-cis">Mulher cisgênero</option>
-                            <option value="hetero-cis">Homem cisgênero</option>
-                            <option value="mulher-trans">Mulher transgênero</option>
-                            <option value="homem-trans">Homem transgênero</option>
-                            <option value="nao-bi">Não binário</option>
-                            <option value="agenero">Agênero</option>
-                            <option value="n-prefere">Prefiro não responder</option>
-                        </select>
-                    </label>
-                    <label>
-                        Orientação sexual:
-                        <select name="orientacao" value={orientacao}>
-                            <option value="heterossexual">Heterossexual</option>
-                            <option value="homossexual">Homossexual</option>
-                            <option value="bissexual">Bissexual</option>
-                            <option value="asssexual">Assexual</option>
-                            <option value="pansexual">Pansexual</option>
-                            <option value="n-prefere">Prefiro não responder</option>
-                        </select>
-                    </label>
-                    <label>
-                        Cor ou raça:
-                        <select name="cor" value={cor}>
-                            <option value="branco">Branco</option>
-                            <option value="preto">Preto</option>
-                            <option value="pardo">Pardo</option>
-                            <option value="indigina">Indigena</option>
-                            <option value="amarelo">Amarelo</option>
-                            <option value="n-prefere">Prefiro não responder</option>
-                        </select>
-                    </label>
-                </div>
-                <CampoTexto
-                    obrigatorio={true}
-                    label="Email"
-                    placeholder="Digite o seu email aqui"
-                    valor={email}
-                    aoAlterar={valor => setEmail(valor)}
-                />
-                <CampoTexto
-                    obrigatorio={true}
-                    label="Senha"
-                    placeholder="Digite a sua senha aqui"
-                    valor={senha}
-                    aoAlterar={valor => setSenha(valor)}
-                />
-            </form>
-        </section>    
-        
-    )
+    const formik = useFormik({
+        initialValues: {
+            nome: '',
+            cidade: '',
+            estado: '',
+            email: '',
+            senha: '', 
+            confsenha: '',
+        },
+      });
+      return (
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="nome">Nome</label>
+          <input
+            id="nome"
+            name="nome"
+            onChange={formik.handleChange}
+            value={formik.values.nome}
+          />
+          <label htmlFor="cidade">Cidade</label>
+          <input
+            id="cidade"
+            name="cidade"
+            onChange={formik.handleChange}
+            value={formik.values.cidade}
+          />
+          <label htmlFor="estado">Estado</label>
+          <input
+            id="estado"
+            name="estado"
+            type="estado"
+            onChange={formik.handleChange}
+            value={formik.values.estado}
+          />
+           <Field name="identidade" as="selecao">
+                <option value="mulher-cis">Mulher cisgênero</option>
+                <option value="homem-cis">Homem cisgênero</option>
+                <option value="mulher-trans">Mulher transgênero</option>
+             </Field>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            onChange={formik.handleChange}
+            value={formik.values.email}
+          />
+          <label htmlFor="senha">Senha</label>
+          <input
+            id="senha"
+            name="senha"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.senha}
+          />
+          <label htmlFor="conf-senha">Confirme sua senha</label>
+          <input
+            id="conf-senha"
+            name="conf-senha"
+            type="password"
+            onChange={formik.handleChange}
+            value={formik.values.confsenha}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      );
 }
 
 export default FormularioCadastro
