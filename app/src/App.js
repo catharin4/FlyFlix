51da21e1e38412b8ac74442454ae1db98a983f7d
Catharina <catharina.bri@gmail.com>
2023-07-18 19:30:48 -0300

    atualizando algumas coisas

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import NavBar from "./Components/Navbar"
 import Home from "./Pages/Home"
 import Cadastro from "./Pages/Cadastro"
+import Login from "./Pages/Login"
 import "./App.css"
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/cadastro' element={<Cadastro />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </>
   )
