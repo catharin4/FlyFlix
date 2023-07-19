59fb215ca32613bf55e0ceac8626737f3e82e933
Catharina <catharina.bri@gmail.com>
2023-07-19 15:16:22 -0300

    land page

--- app/src/App.js
+++ app/src/App.js
@@ -4,12 +4,14 @@ import Home from "./Pages/Home"
 import Cadastro from "./Pages/Cadastro"
 import Login from "./Pages/Login"
 import "./App.css"
+import LandPage from "./Pages/LandPage"
 
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
+        <Route path="/landpage" element={<LandPage />} />
         <Route path='/' element={<Home />} />
         <Route path='/cadastro' element={<Cadastro />} />
         <Route path='/login' element={<Login />} />
---
+++ app/src/Pages/LandPage/index.js
@@ -0,0 +1,13 @@
+import Header from "../../Components/Header"
+import Footer from "../../Components/Footer"
+import BoasVindas from "../../Components/BoasVindas"
+
+function LandPage() {
+    <>
+    <Header />
+    <BoasVindas />
+    <Footer />
+    </>
+}
+
+export default LandPage
