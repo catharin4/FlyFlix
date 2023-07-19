91a5d1996d0ba140d8f4cd84cea61f95b46073f7
Glauce "Aylinha <100098192+aylastankovsky@users.noreply.github.com>
2023-07-18 22:51:50 -0300

    att erros

--- app/src/index.js
+++ app/src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- <React.StrictMode>
+const rootNode = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
     <BrowserRouter>
-       <App />
+      <App />
     </BrowserRouter>
- </React.StrictMode>
+  </React.StrictMode>,
+  rootNode
 );
